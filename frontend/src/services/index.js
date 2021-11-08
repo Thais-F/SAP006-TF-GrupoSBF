827417f59fa5900f659bffda71c3319edd0e93d3
Thalita Neves <thalita.neves24@gmail.com>
2021-11-08 14:24:04 -0300

    routes e fetch de promoções

--- frontend/src/services/index.js
+++ frontend/src/services/index.js
@@ -1,19 +1,14 @@
-import React from "react";
-
-export const getPromotions = () => {
+export const getPromotions = async() => {
 
     const myInit = {
         method: "GET",
         headers: {
-          "Content-type": "application/json; charset=UTF-8",
+          "Content-type": "application/json",
         },
     };
 
-    return fetch("http://localhost:3000/promotions", myInit)
-    .then(function (response) {
-        return response.json();
-      })
-      .catch((error) => {
-        return error;
-      });  
+    const response = await fetch("http://localhost:3000/promotions", myInit);
+
+    const data = await response.json();
+    return data
 };
