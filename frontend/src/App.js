45896e74a46dea3f4c75ce462905ec4f08c6c0ee
Lediane <lediane141@gmail.com>
2021-11-08 15:40:08 -0300

    componente header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,15 +5,20 @@ import Home from './pages/home/Home';
 import Search from './pages/search/Search';
 import Cart from './pages/cart/Cart';
 import NotFound from './pages/notFound/NotFound';
+import Header from '../src/components/header/Header';
 
 function App() {
   return (
+    <div>
+      <Header />
+
     <Routes>
       <Route path="/" element={<Home/>}/>
       <Route path="/busca" element={<Search/>}/>
       <Route path="/carrinho" element={<Cart/>}/>
       <Route path="*" element={<NotFound/>}/>
     </Routes>
+    </div>
   );
 }
 
---
+++ frontend/src/components/inputSearch/InputSearch.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './index.css';
+
+function Input({
+    value,
+    type,
+    className,
+    name,
+    placeholder,
+    onChange,
+}) {
+    return (
+        <input
+            className={className}
+            type={type}
+            placeholder={placeholder}
+            onChange={onChange}
+            value={value}
+            name={name}
+        />
+    )
+}
+
+export default Input;
