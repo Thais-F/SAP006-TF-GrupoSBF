f64d47031353a115856455a442c8ecc68dcd7d88
giuthamie <giumie@hotmail.com>
2021-11-08 20:13:39 -0300

    Component EmptyCart Desktop

--- frontend/src/components/emptyCart/EmptyCart.js
+++ frontend/src/components/emptyCart/EmptyCart.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import carrinho from '../../assets/images/carrinho.jpeg';
+import carrinho from '../../assets/images/carrinho.png';
 import './EmptyCart.css';
 
 
@@ -7,9 +7,11 @@ const EmptyCart = () => {
     return (
         <div className="principal-container">
             <img src={carrinho} className='cart-img' alt='imagem carrinho' />
-            <p className="title">Seu carrinho está vazio...</p>
-            <p className="message">São milhares de produtos para você escolher Escolha seus produtos e adicione em seu carrinho</p>
-            
+            <div className="secundary-container">
+                <p className="title">Seu carrinho está vazio...</p>
+                <p className="message">São milhares de produtos para você escolher Escolha seus produtos e adicione em seu carrinho</p>
+                <button className="btn" onClick={() => alert('rota direcionará para home')}>Comece a comprar</button>
+            </div>
         </div>
     )
 }
