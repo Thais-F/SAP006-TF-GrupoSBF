a5f11bfdc816883546b6b32fa883873e97680cec
Liziane Garcia <lizianegarciaa@gmail.com>
2021-11-08 22:24:12 -0300

    produto parcialmente estilizado

--- frontend/src/components/product/Product.js
+++ frontend/src/components/product/Product.js
@@ -1,5 +1,8 @@
 import{ React, useEffect, useState } from 'react';
 import { getProducts } from '../../services';
+// import Rating from './Rating';
+import './product.css';
+
 
 const Product = () => {
   const [products, setProducts] = useState([]);
@@ -8,16 +11,29 @@ const Product = () => {
       async function getPromos() {
           const product = await getProducts()
           setProducts(product)
-          console.log(product)
           return product
       }
       getPromos()        
     }, []);
 
+    
   return (
       <div>
           {products.map((items) => (
-           <h1>{items.name}</h1>
+            <div key={items.id} className="card">
+              <p className="discount"> {items.discount === "null" ? '' : items.discount}</p>
+              <img className="img" src={items.image} alt={items.name}/>
+             <p className="shipping"> {items.freeShipping === "true" ? '' : (<p>Frete Grátis</p>)}</p>
+           <div className="card-body">
+           <h2>{items.name}</h2>
+           <div className="price">
+           <p className="old-price"> {items.oldPrice === "null" ? '' : items.oldPrice}</p>
+           R$ {items.price}
+             </div>
+            {/* <Rating/> */}
+            <h3>{items.colors} cores</h3>
+           </div>
+           </div>
         ))}
       </div>
   )
